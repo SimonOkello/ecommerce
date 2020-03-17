438f29a4e0e3ccb0e5e352c5517d66da5d949f89
SimonOkello <simonokello93@gmail.com>
2020-03-17 16:10:46 +0300

    Finished checkout page

--- eshop/models.py
+++ eshop/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
+from django_countries.fields import CountryField
 
 # Create your models here.
 
@@ -70,6 +71,19 @@ class OrderItem(models.Model):
             return self.get_total_discount_item_price()
         return self.get_total_item_price()
 
+# Billing Address
+
+class BillingAddress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    country = CountryField(multiple=False)
+    zip = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -78,6 +92,7 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
+    billing_address = models.ForeignKey(BillingAddress, on_delete = models.SET_NULL, blank = True, null = True)
 
     def __str__(self):
         return str(self.user)
@@ -87,3 +102,4 @@ class Order(models.Model):
         for order_item in self.items.all():
             total += order_item.get_final_price()
         return total
+
--- eshop/views.py
+++ eshop/views.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 from django.views.generic import ListView, DetailView, View
 from . forms import CheckoutForm
 from django.utils import timezone
@@ -42,11 +42,39 @@ class CheckoutView(View):
     def post(self, *args, **kwargs):
         #form
         form = CheckoutForm(self.request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-            print("The form is valid")
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            if form.is_valid():
+                street_address =form.cleaned_data.get('street_address')
+                apartment_address        =form.cleaned_data.get('apartment_address')
+                country =form.cleaned_data.get('country')
+                zip = form.cleaned_data.get('zip')
+                # TODO: add functionality for these fields.
+                # same_shipping_address =form.cleaned_data.get('same_shippping_address')
+                # save_info =form.cleaned_data.get('save_info')
+                payment_option = form.cleaned_data.get('payment_option')
+                billing_address = BillingAddress(
+                    user=self.request.user,
+                    street_address=street_address,
+                    apartment_address=apartment_address,
+                    country = country,
+                    zip=zip,
+                   
+                )
+                billing_address.save()
+                order.billing_address = billing_address
+                order.save()
+                # TODO: add a redirect to select payment option.
+                return redirect('checkout')
+            messages.warning(self.request, 'Failed to checkout')
             return redirect('checkout')
     
+
+        except ObjectDoesNotExist:
+            messages.error(self.request, 'You do not have an active order')
+            return redirect('order-summary')
+
+        
 
 # Add To Cart View
 
